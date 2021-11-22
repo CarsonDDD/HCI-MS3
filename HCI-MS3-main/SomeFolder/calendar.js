5f41823abc5c2417ac6e3756fc8d6bd40cabfc21
Carson <carson-dickinson@hotmail.com>
2021-11-22 11:43:42 -0600

    fixed height issue

--- HCI-MS3-main/SomeFolder/calendar.js
+++ HCI-MS3-main/SomeFolder/calendar.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+      height: 650,
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
